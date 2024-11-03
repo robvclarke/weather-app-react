9340e3e20002357ef772805608871ed350c2d24a
Robert Clarke <robvclarke@gmail.com>
2024-11-03 21:40:48 +0000

    Add Logo icon to navbar and make clicking on it refresh homepage

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,17 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
+import logo from "../assets/Hi-Res-Logo.svg"; // Adjust path if necessary
 
 function Navbar() {
   return (
     <nav className="navbar">
-      <h2 className="navbar__logo">Clarke Weather Inc.</h2>
+      {/* Logo and text wrapper */}
+      <div className="navbar__logo-wrapper">
+        <Link to="/" onClick={() => window.location.reload()}>
+          <img src={logo} alt="Clarke Weather Inc. Logo" className="navbar__logo-image" />
+        </Link>
+        <h2 className="navbar__logo-text">Clarke Weather Inc.</h2>
+      </div>
       <ul className="navbar__links">
         <li>
           <NavLink to="/" className={({ isActive }) => (isActive ? "active" : "")} end>
