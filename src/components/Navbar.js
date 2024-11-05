0927cdb575ec0e66c03c21a05c4e8e02a568a15d
Robert Clarke <robvclarke@gmail.com>
2024-11-05 16:18:27 +0000

    Fix issues with navbar functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,22 @@
 import React from "react";
-import { NavLink, Link } from "react-router-dom";
+import { NavLink, Link, useLocation } from "react-router-dom";
 import logo from "../assets/GradientLogo.png"; // Adjust path if necessary
 
 function Navbar() {
+  const location = useLocation();
+
+  const handleLogoClick = () => {
+    // If already on the homepage, refresh the page
+    if (location.pathname === "/") {
+      window.location.reload();
+    }
+  };
+
   return (
     <nav className="navbar">
       {/* Logo and text wrapper */}
       <div className="navbar__logo-wrapper">
-        <Link to="/" onClick={() => window.location.reload()}>
+        <Link to="/" onClick={handleLogoClick}>
           <img src={logo} alt="Clarke Weather Inc. Logo" className="navbar__logo-image" />
         </Link>
       </div>
